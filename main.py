c1a9ad49d416eb5089364090e2f2a7715b03b65a
FloppaG <93928325+FloppaG@users.noreply.github.com>
2023-06-29 21:23:14 +0000

    Optimized program

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+print("Importing packages")
 import os
+os.system("clear")
 
 cmd1 = "flop help"
 cmd1alt = "flop list"
 cmd2 = "flop del"
-usr = input("Enter a command: ")
-if usr == cmd1:
-    print("help/list - Shows a list of commands")
-else:
-  if usr == cmd1alt:
-    print("help/list- Shows a list of commands")
+
+while 1 < 2:
+  usr = input("Enter a command: ")
+  if usr == cmd1:
+      print("help/list - Shows a list of commands/n/del - Removes a file ")
   else:
-    delete_file = input("Enter the path of the file that you want to delete: ")
-    os.remove(delete_file)
+    if usr == cmd1alt:h
+    print("help/list- Shows a list of commands\n\ndel - Removes a file")
+else:
+  if usr == cmd2:h
+  delete_file = input("Enter the path of the file that you want to delete: ")
+  os.remove(delete_file)
