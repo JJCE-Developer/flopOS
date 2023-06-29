914805f681f8ddb4c538af1336c52c36d40e3124
JezreelErickson <10326500-JezreelErickson@users.noreply.replit.com>
2023-06-29 18:15:55 +0000

    Did stuff

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 
-cmd1 = "help"
-cmd2 = "del"
+cmd1 = "flop help"
+cmd1alt = "flop list"
+cmd2 = "flop del"
 usr = input("Enter a command: ")
 if usr == cmd1:
-    print("help - Shows a list of commands")
+    print("help/list - Shows a list of commands")
 else:
-    if usr == cmd2:
-        delete_file = input("Enter the name of the file that you want to delete")
-        os.remove(delete_file)
+  if usr == cmd1alt:
+    print("help/list- Shows a list of commands")
+  else:
+    delete_file = input("Enter the path of the file that you want to delete: ")
+    os.remove(delete_file)
